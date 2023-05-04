7d5e148a6448d1ff9ff5d25b8df3c20671c075a2
onkar <onkarvasav@gmail.com>
2023-05-04 23:41:07 +0530

    cc : Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,6 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// // import "./Login.css";
-
-// const Login = ({ onLogin }) => {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const navigate = useNavigate();
-
-//   const handleUsernameChange = (e) => {
-//     setUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       navigate("/movies");
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   return (
-//     <div className="login-container">
-//       <form onSubmit={handleSubmit}>
-//         <h1>Login</h1>
-//         <div className="input-container">
-//           <label htmlFor="username">Username:</label>
-//           <input
-//             type="text"
-//             id="username"
-//             value={username}
-//             onChange={handleUsernameChange}
-//           />
-//         </div>
-//         <div className="input-container">
-//           <label htmlFor="password">Password:</label>
-//           <input
-//             type="password"
-//             id="password"
-//             value={password}
-//             onChange={handlePasswordChange}
-//           />
-//         </div>
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Login.scss";
 
 function Login() {
   const navigate = useNavigate();
@@ -65,38 +8,36 @@ function Login() {
   const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    // Mock authentication function that always returns the default token
     const token = "Wookie2019";
 
-    // Store the token in local storage for persistence
     localStorage.setItem("token", token);
 
-    // Navigate to the movies list page
     navigate("/movies");
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Username:
+    <div className="loginwrapper">
+      <div className="logincontainer">
+        <h1>Welcome</h1>
+        <h3>Sign in using</h3>
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+          <label>Username</label>
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-        </label>
-        <label>
-          Password:
+
+          <label>Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-        </label>
-        <button onClick={handleLogin}>Login</button>
-      </form>
+
+          <button onClick={handleLogin}>Continue</button>
+        </form>
+      </div>
     </div>
   );
 }
